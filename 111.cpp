144e7c688eb729a27d3ea78fac2e92045beb21ca
zxy01zxy <101505186+zxy01zxy@users.noreply.github.com>
2022-04-01 09:57:52 +0800

    Add files via upload

---
+++ 111.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void fun(char *s,char t[])  
+while(*s!='\0')    
+{if(*s%2==1||i%2==0)     
+{t[j]=*s;j++;}    
+s++;i++;}
+t[j]='\0';       
+}
+main()
+{
+char a1[100],a2[100];     
+printf("«Î ‰»Î“ª¥Æ◊÷∑˚£∫");
+scanf("%s",a1);    
+fun(a1,a2);     
+printf("%s",a2);   
+}
+
